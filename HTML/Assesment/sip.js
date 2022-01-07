242cdf311f1f3e444f0da2a19c134f9b1c087015
sourabh12sinha <sourabh12sinha@gmail.com>
2022-01-07 14:13:07 +0530

    HTML and MySQL Assesment

---
+++ HTML/Assesment/sip.js
@@ -0,0 +1,10 @@
+function calculator()
+{
+   
+    var x = document.getElementById("month").value;
+    var y = document.getElementById("year").value;
+    var z=(x*12*25)*y;
+    alert(z);
+
+
+}
---
+++ HTML/first.js
@@ -0,0 +1,26 @@
+function hello(){
+    alert("hello javascript");
+    console.log("this is javascript");
+
+}
+function add(a, b)
+{
+    alert (a+b);
+    onsole.log("add")
+} 
+function printdata(){
+    let p=prompt("please enter your name","place name");
+    if(p!=null)
+    {
+        console.log(p);
+    }
+    
+}
+function print(){
+    let p=prompt("please enter your name","place name");
+    if(p!=null)
+    {
+       document.getElementById("demo").innerHTML
+    }
+    
+}
